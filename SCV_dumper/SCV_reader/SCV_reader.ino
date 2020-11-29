2efd723713751337ea6126cc91c4980b958a7b79
Bjorn Nah <bjorn.nah@gmail.com>
2020-11-29 19:02:23 +0100

    write /erase functions wip

--- SCV_dumper/SCV_reader/SCV_reader.ino
+++ SCV_dumper/SCV_reader/SCV_reader.ino
@@ -3,23 +3,24 @@
 
 const int serialSpeed = 9600;
 const int clockPin = 12;
-const int dataPin = 13;
-const int cePin = 11;
+const int adressPin = 13;
+const int oePin = 11;
+const int wePin = 10;
 unsigned int maxAddress = 0x2000;
 const int gcDataBit[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
 
 
 // Shifts a 16-bit value out to a shift register.
 // Parameters:
-//   dataPin - Arduino Pin connected to the data pin of the shift register.
+//   adressPin - Arduino Pin connected to the data pin of the shift register.
 //   clockPin - Arduino Pin connected to the data clock pin of the shift register.
 //----------------------------------------------------------------------------------
 void shiftOut16(int bitOrder, int value)
 {
   // Shift out highbyte for MSBFIRST
-  shiftOut(dataPin, clockPin, bitOrder, (bitOrder == MSBFIRST ? (value >> 8) : value));  
+  shiftOut(adressPin, clockPin, bitOrder, (bitOrder == MSBFIRST ? (value >> 8) : value));  
   // shift out lowbyte for MSBFIRST
-  shiftOut(dataPin, clockPin, bitOrder, (bitOrder == MSBFIRST ? value : (value >> 8)));
+  shiftOut(adressPin, clockPin, bitOrder, (bitOrder == MSBFIRST ? value : (value >> 8)));
 }
 
 // Read data lines
@@ -35,13 +36,13 @@ void ReadDataLines()
   boolean dataBits[8];
   char byteReadHex[4];
 
-  digitalWrite(cePin,LOW);
+  digitalWrite(oePin,LOW);
   delayMicroseconds(1);
   for(int currentBit = 0; currentBit < 8; currentBit++)
   {
     dataBits[currentBit] = digitalRead(gcDataBit[currentBit]);
   }
-  digitalWrite(cePin,HIGH);
+  digitalWrite(oePin,HIGH);
 
   highNibble = (dataBits[7] << 3) + (dataBits[6] << 2) + (dataBits[5] << 1) + dataBits[4];
   lowNibble = (dataBits[3] << 3) + (dataBits[2] << 2) + (dataBits[1] << 1) + dataBits[0];
@@ -49,6 +50,32 @@ void ReadDataLines()
   Serial.write(cHexLookup[highNibble]);
   Serial.write(cHexLookup[lowNibble]);
   //Serial.println();
+}
+
+// Write data lines
+//----------------------------------------------------------------------------------
+void WriteDataLines(byte data)
+{
+  
+}
+
+void setByte(byte out)
+{
+  for(int currentBit = 0; currentBit<8;currentBit++)
+  {
+    digitalWrite(gcDataBit[currentBit], bitRead(out ,currentBit));
+  }
+}
+
+void writeByte(byte data, unsigned long address)
+{
+  digitalWrite(oePin,HIGH);
+  digitalWrite(wePin,HIGH);
+  shiftOut16(MSBFIRST, address);
+  setByte(data);
+  digitalWrite(oePin,LOW);
+  delayMicroseconds(1);
+  digitalWrite(wePin,LOW);  
 }
 
 // Read all of the data from the cartridge.
@@ -67,6 +94,21 @@ void ReadCartridge()
   }
   
   Serial.println(":END");
+}
+
+// Errase data from cartrige.
+//----------------------------------------------------------------------------------
+void EraseCartridge()
+{
+  setDataPinOut();
+  writeByte(0xAA, 0x5555);
+  writeByte(0x55, 0x2AAA);
+  writeByte(0x80, 0x5555);
+  writeByte(0xAA, 0x5555);
+  writeByte(0x55, 0x2AAA);
+  writeByte(0x10, 0x5555);
+  delay(100);
+  setDataPinOut();
 }
 
 // Returns the next line from the serial port as a String.
@@ -106,6 +148,28 @@ void ReadyForCommand()
   Serial.println("READY:");
 }
 
+// Setup zone
+//----------------------------------------------------------------------------------
+void setDataPinOut()
+{
+  for(int pinInt = 0; pinInt < 8; pinInt++){
+    pinMode(gcDataBit[pinInt], OUTPUT);
+  }
+}
+
+void setDataPinIn()
+{
+  for(int pinInt = 0; pinInt < 8; pinInt++){
+    pinMode(gcDataBit[pinInt], INPUT_PULLUP);
+  }
+}
+void setCtrlPins()
+{
+  pinMode(clockPin, OUTPUT);
+  pinMode(adressPin, OUTPUT);
+  pinMode(oePin, OUTPUT);
+  pinMode(wePin, OUTPUT);
+}
 
 void setup() {
   unsigned int initAddress = 0;
@@ -114,12 +178,8 @@ void setup() {
   Serial.begin(serialSpeed);
 
   // Setup pins
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-  pinMode(cePin, OUTPUT);
-  for(int pinInt = 0; pinInt < 8; pinInt++){
-    pinMode(gcDataBit[pinInt], INPUT_PULLUP);
-  }
+  setCtrlPins();
+  setDataPinIn();
 
     while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only.
